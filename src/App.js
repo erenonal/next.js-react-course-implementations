66673caeadf8aec2d0ce664f848353dfa931d119
Eren Önal <75484033+erenonal@users.noreply.github.com>
2023-10-08 02:30:17 +0300

    props, define functions, state

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1> My Todos</h1>
+      <Todo text='Learn React'/>
+      <Todo text='Develop React'/>
+      <Todo text='Build React'/>
+    </div>
+  );
+}
+export default App;
