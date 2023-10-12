f334607abd614ceee0e6a3350f865cf7f3873f9a
Eren Önal <75484033+erenonal@users.noreply.github.com>
2023-10-12 23:43:15 +0300

    basic header is created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-
+import { Route, Switch } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+import MainNavigaton from "./components/layout/MainNavigation";
 function App() {
   return (
-    <div className="App">
-      Start Something New
+    <div>
+      <MainNavigaton/>
+        <Switch>
+          <Route path="/" exact>
+            <AllMeetupsPage></AllMeetupsPage>
+          </Route>
+          <Route path="/new-meetup" exact>
+            <NewMeetupPage></NewMeetupPage>
+          </Route>
+          <Route path="/favorites" exact>
+            <FavoritesPage>Favorites</FavoritesPage>
+          </Route>
+        </Switch>
     </div>
   );
 }
